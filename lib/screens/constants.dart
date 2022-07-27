bf80f9ba8fa548b52175e1030c14e83618fe3440
Shreyam Gupta <shreyamgupta13asia@gmail.com>
2022-07-28 00:56:47 +0530

    Updated Questions

--- lib/screens/constants.dart
+++ lib/screens/constants.dart
@@ -4,15 +4,15 @@ import 'package:quiz_app/models/question.dart';
 
 List questions = [
   
-  Question(question: "capital of india is ?",
-  options: ["Chandigarh", "Delhi", "Kerala", "Qutan"],
+  Question(question: "Which is the world's happiest country?",
+  options: ["Japan", "Singapore", "Finland", "Switzerland"],
+  correctOption: 2
+  ),
+  Question(question: "What Netflix show had the most streaming views in 2021?",
+  options: ["Peaky Blinders", "Squid Game", "Suits", "Breaking Bad"],
   correctOption: 1
   ),
-  Question(question: "capital of Haryana is ?",
-  options: ["Chandigarh", "Delhi", "Kerala", "Qutan"],
-  correctOption: 0
-  ),
-  Question(question: "capital of Punjab is ?",
+  Question(question: "What is the Capital of Punjab?",
   options: ["Chandigarh", "Delhi", "Kerala", "Qutan"],
   correctOption: 0
   )
